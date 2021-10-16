551bcaa61ad03e29a5a38d83c8f60bb6dba2a041
Christian Lachapelle <devworks8@gmail.com>
2021-10-16 15:04:51 -0400

    Completed Assignment

--- Assignment3/Birds.cs
+++ Assignment3/Birds.cs
@@ -6,6 +6,12 @@ namespace A3.Birds
 	class Bird
 	{
 		public static Random Rand = new Random(1);
+
+		protected Egg GetEgg(double minSize, double maxSize, double brokenChance, Egg.Colors color)
+        {
+			return Rand.NextDouble() <= brokenChance ? new BrokenEgg(Rand.NextDouble() * (maxSize - minSize) + minSize, color) : new Egg(Rand.NextDouble() * (maxSize - minSize) + minSize, color);
+		}
+
 		public virtual Egg[] LayEggs(int numEggs)
 		{
 			Console.Error.WriteLine("Bird.LayEggs should not be called!");
@@ -15,11 +21,31 @@ namespace A3.Birds
 
 	class Chicken : Bird
 	{
-		// TODO: Implement this class
-	}
+        public override Egg[] LayEggs(int numEggs)
+        {
+			Egg[] eggs = new Egg[numEggs];
+
+			for(int i = 0; i < numEggs; ++i)
+            {
+				eggs[i] = GetEgg(2, 4, 0.25, Egg.Colors.brown);
+            }
+			
+			return eggs;
+        }
+    }
 
 	class Ostrich : Bird 
 	{
-		// TODO: Implement this class
+		public override Egg[] LayEggs(int numEggs)
+        {
+			Egg[] eggs = new Egg[numEggs];
+
+			for(int i = 0; i < numEggs; ++i)
+            {
+				eggs[i] = GetEgg(10, 15, 0.45, Egg.Colors.speckled);
+            }
+
+			return eggs;
+        }
 	}
 }
